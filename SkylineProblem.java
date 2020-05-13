cba1e7aff8b427e8de99766626f7727aa242f275
thomaz-s <63955736+thomaz-s@users.noreply.github.com>
2020-05-12 21:43:13 -0300

    Update SkylineProblem
    It remains unsolved

--- SkylineProblem.java
+++ SkylineProblem.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package PacoteProblemas;
+package Desafios;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,49 +6,123 @@ import java.util.Scanner;
 /**
  *
  * @author thomas
- * https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=41
+ * UNSOLVED
+ * Link to the problem: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=41
  */
 public class SkylineProblem {
+    
+    private static ArrayList<Dot>dots = new ArrayList<>();
+    private static ArrayList<String>inputs = new ArrayList<>();
+    private static int first;
+    private static int last;
+    
     public static void main(String[] args) {
-                     
-        //inserção de valores
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Ponto>pontos = new ArrayList<>();
-        String input;
-        Ponto ponto = new Ponto();
-        input = scanner.nextLine();
-        while(!input.equals("")){
-            ponto.setX(Integer.parseInt(input.split("\\s+")[0]));
-            ponto.setY(Integer.parseInt(input.split("\\s+")[1]));
-            pontos.add(ponto);
-            ponto.setX(Integer.parseInt(input.split("\\s+")[0]));
-            pontos.add(ponto);
-            input = scanner.nextLine();
-        }
         
-        for (Ponto p : pontos){
-            System.out.println(p.getX()+" "+p.getY());
+        readInputs();
+        createLines();
+        removeDuplicate();
+        showNotDuplicates();
+        
+    }
+
+    private static void readInputs() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do{
+            input = scanner.nextLine();
+            inputs.add(input);
+        }while(!input.equals(""));
+        inputs.remove("");
+    }
+
+    private static void createLines(){
+        first = Integer.parseInt(inputs.get(0).split("\\s+")[0]);
+        last = Integer.parseInt(inputs.get(inputs.size()-1).split("\\s+")[2]);
+        
+        for (String input : inputs){
+            int l = Integer.parseInt(input.split("\\s+")[0]);
+            int h = Integer.parseInt(input.split("\\s+")[1]);
+            int r = Integer.parseInt(input.split("\\s+")[2]);
+            
+            if (l < first) {
+                first = l;
+            }
+            
+            if (r > last) {
+                last = r;
+            }
+            
+            Dot dot;
+           
+            //criar paredes 
+            for (int y = 0; y <= h; y++){
+                //esquerda
+                dot = new Dot();
+                dot.setPosition(l, y);
+                dots.add(dot);
+
+                //direita
+                dot = new Dot();
+                dot.setPosition(r, y);
+                dots.add(dot);
+            }
+
+            //criar teto
+            for (int x = l; x <= r; x++){
+                dot = new Dot();
+                dot.setPosition(x, h);
+                dots.add(dot);
+            }
         }
     }
+
+    private static void removeDuplicate() {
+        for (int i = 0; i < dots.size()-1; i++){
+            for (int j = i+1; j < dots.size(); j++){
+                if (dots.get(i).getPosition().equals(dots.get(j).getPosition())){
+                    dots.get(i).duplicate = true;
+                }
+            }
+        }
+    }
+
+    private static void showNotDuplicates() {
+        for (int i = 0; i < dots.size(); i++){
+            if (!dots.get(i).duplicate){
+                System.out.println(i + ": " + dots.get(i).getPosition());
+            }
+        }
+    }
+    
 }
 
-class Ponto{
+class Dot{
     private int x;
     private int y;
+    boolean duplicate = false;
     
-    void setX(int x){
+    void setPosition(int x, int y){
         this.x = x;
-    }
-    
-    void setY(int y){
         this.y = y;
     }
-    
-    int getX(){
-        return x;
+
+    String getPosition(){
+        return this.x + ", " + this.y;
     }
     
-    int getY(){
-        return y;
+    String nextUp(){
+        return this.x + ", " + (this.y+1);
+    }
+    
+    String nextDown(){
+        return this.x + ", " + (this.y-1);
+    }
+    
+    String nextLeft(){
+        return (this.x-1) + ", " + (this.y);
+    }
+    
+    String nextRight(){
+        return (this.x+1) + ", " + (this.y);
     }
 }
